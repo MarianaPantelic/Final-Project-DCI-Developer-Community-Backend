21e52d338a64625e469dcc138cf41ef57658c6c4
Mariana Pantelic <mari27line@yahoo.com>
2021-08-05 15:21:10 +0200

    updated app.js, connected to mongodb

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+const mongoose = require("mongoose");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const blogRouter = require("./routes/blog");
+const profileRouter = require("./routes/profile");
+
+const { setCors } = require("./middleware/security");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//CONNECTING WITH MONGODB
+mongoose.connect(
+  `mongodb+srv://dbMariana:test1234@cluster0.u2n6q.mongodb.net/FinalProjectDatabase?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+mongoose.connection.on("error", console.error);
+mongoose.connection.on("open", () =>
+  console.log("Database connection established")
+);
+
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(setCors);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/blog", blogRouter);
+app.use("/profile", profileRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 module.exports = app;
