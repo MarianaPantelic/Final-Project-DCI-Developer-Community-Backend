c86bcca6dd2fdb3448a4a94193b8ea7f4a531a54
Florin <fiathf@yahoo.com>
2021-08-11 14:38:04 +0200

    added forum components update

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use("/users", usersRouter);
 app.use("/profile", profileRouter);
 app.use("/blogs", blogsRouter);
 app.use("/questions", questionsRouter);
-app.use("/answers", answersRouter);
+
 
 app.use((err, req, res, next) => {
   console.log(err);
